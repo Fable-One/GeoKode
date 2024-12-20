18f632291c63655e6ef9a38e6bf225917832e6c2
Devin D <58495296+DevinDuricka@users.noreply.github.com>
2024-12-19 22:13:20 -0800

    incremented Java Version

--- GeoKode/build.gradle.kts
+++ GeoKode/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "17"
             }
         }
         publishLibraryVariants("release")
@@ -47,8 +47,8 @@ android {
         minSdk = 21
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 dependencies {
